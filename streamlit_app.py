3959ad5f68dbb345db41bdfbaf808217a7d16cc1
Vojtech Huser <vojtech.huser@pfizer.com>
2024-01-19 12:43:22 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,18 @@
-import altair as alt
-import numpy as np
 import pandas as pd
+import pygwalker as pyg
+
 import streamlit as st
+import streamlit.components.v1 as components
 
-"""
-# Welcome to Streamlit!
+title = 'Top Informatics PIs'
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.set_page_config(
+    page_title=title,
+    layout='wide'
+)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.title(title)
+st.markdown(" <style> div[class^='block-container'] { padding-top: 2rem; } </style> ", unsafe_allow_html=True)
+df = pd.read_csv('01-PIs.csv.gz')
+pyg_html = pyg.to_html(df)
+components.html(pyg_html, height=1000, scrolling=True)
