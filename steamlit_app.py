8d6fe66705d2ed66a4ece49961b010b29104d019
Vojtech Huser <vojtech.huser@pfizer.com>
2024-01-19 12:39:08 -0500

    Add files via upload

---
+++ steamlit_app.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pygwalker as pyg
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+title = 'Top Informatics PIs'
+
+st.set_page_config(
+    page_title=title,
+    layout='wide'
+)
+
+st.title(title)
+st.markdown(" <style> div[class^='block-container'] { padding-top: 2rem; } </style> ", unsafe_allow_html=True)
+df = pd.read_csv('01-PIs.csv.gz')
+pyg_html = pyg.to_html(df)
+components.html(pyg_html, height=1000, scrolling=True)
